c545f0eb516694dc98f622be1befe0ddb0077736
przrk <76454345+przrk@users.noreply.github.com>
2021-05-06 15:17:35 +0300

    add

--- script.js
+++ script.js
@@ -8,52 +8,75 @@ button.forEach(x => {
 let firstNumber;
 let secondNumber;
 let operator;
+let newOperator;
+
 button.forEach(x => {
-  x.addEventListener('click', e => {
-    if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-      .includes(e.target.textContent) && firstNumber === undefined
-    ) {
+  x.addEventListener('click', () => {
+    if (/\d/.test(x.textContent) && firstNumber !== 0 && !firstNumber) {
+      if (secondNumber === 'new') {
+        text.textContent = '';
+        secondNumber = undefined;
+      }
       x.classList.remove('pressed');
-      text.textContent += e.target.textContent;
-    } else if (['+', '-', '×', '÷'].includes(e.target.textContent)) {
-      operator = e;
+      text.textContent += x.textContent;
+      if (/0\d/.test(text.textContent)) {
+        text.textContent = x.textContent;
+      }
+    } else if (['+', '-', '×', '÷'].includes(x.textContent)
+      && text.textContent && !operator) {
+      operator = x;
       firstNumber = +text.textContent;
-    } else if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-      .includes(e.target.textContent) && secondNumber === undefined
-    ) {
+    } else if (/\d/.test(x.textContent) && operator) {
       if (firstNumber === +text.textContent) {
         text.textContent = '';
       }
-      if (operator.target.className === 'button pressed') {
-        operator.target.classList.remove('pressed');
+      if (operator.className === 'button operator pressed') {
+        operator.classList.remove('pressed');
       }
       x.classList.remove('pressed');
-      text.textContent += e.target.textContent;
-    } else if (['='].includes(e.target.textContent)) {
-      operator = operator.target.textContent;
+      text.textContent += x.textContent;
+    } else if (x.textContent === '=' && operator
+      && operator.className !== 'button operator pressed') {
+      operator = operator.textContent;
       secondNumber = +text.textContent;
       x.classList.remove('pressed');
       text.textContent = '';
-      operate(operator, firstNumber, secondNumber);
+      newOperator = undefined;
+      operate();
+    } else if (['+', '-', '×', '÷'].includes(x.textContent)
+      && firstNumber && operator.className !== 'button operator pressed') {
+      operator = operator.textContent;
+      secondNumber = +text.textContent;
+      text.textContent = '';
+      newOperator = x;
+      operate();
+    } else if (['◄'].includes(x.textContent)) {
+      text.textContent = text.textContent.slice(0, text.textContent.length - 1);
+      x.classList.remove('pressed');
+    } else if (/./.test(x.textContent)) {
+      x.classList.remove('pressed');
     }
   });
 });
-function operate(operator, firstNumber, secondNumber) {
-  return operator === '+' ? add(firstNumber, secondNumber)
-    : operator === '-' ? subtract(firstNumber, secondNumber)
-      : operator === '×' ? multiply(firstNumber, secondNumber)
-        : divide(firstNumber, secondNumber);
-
-}
-function add(firstNumber, secondNumber) {
-  text.textContent = firstNumber + secondNumber;
-}
-function subtract(firstNumber, secondNumber) {
-  text.textContent = firstNumber - secondNumber;
-}
-function multiply(firstNumber, secondNumber) {
-  text.textContent = firstNumber * secondNumber;
-}
-function divide(firstNumber, secondNumber) {
-  text.textContent = firstNumber / secondNumber;
+function operate() {
+  if (operator === '+') {
+    text.textContent = firstNumber + secondNumber;
+  } else if (operator === '-') {
+    text.textContent = firstNumber - secondNumber;
+  } else if (operator === '×') {
+    text.textContent = firstNumber * secondNumber;
+  } else if (operator === '÷') {
+    if (secondNumber === 0) {
+      secondNumber = 1;
+    }
+    text.textContent = (firstNumber / secondNumber).toFixed(2);
+  }
+  if (['+', '-', '×', '÷'].includes(operator)) {
+    firstNumber = +text.textContent;
+    operator = newOperator;
+  }
+  if (!newOperator) {
+    firstNumber = undefined;
+    secondNumber = 'new';
+  }
 }
